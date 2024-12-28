959d871ce1ebeee5bfbff27aaaf1b6635c358e3a
Orlando <orlandofelixperez2003@gmail.com>
2024-12-28 10:29:00 -0300

    funcionalidad del front

--- Js/index.js
+++ Js/index.js
@@ -5,3 +5,23 @@ const links = document.querySelector('ul');
 menu.addEventListener("click", () =>{
     links.classList.toggle("mobile-menu")
 });  
+
+
+// Inicializar EmailJS
+emailjs.init('LYssx_RlQ23JTQYQd'); // Reemplaza con tu ID de usuario EmailJS
+
+// Configuración del envío del formulario
+
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    emailjs
+        .sendForm('service_0rfxl7j', 'template_m6q3kcl', this)
+        .then(() => {
+            alert('Mensaje enviado correctamente');
+            location.reload(); // Recargar la página
+        })
+        .catch((err) => {
+            alert('Error al enviar el mensaje: ' + JSON.stringify(err));
+        });
+});
